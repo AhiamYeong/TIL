8931e29bc45bf207a12c954da484d2a5ca8d44c4
AhiamYeong <toktto@gmail.com>
2025-03-18 19:50:05 +0900

    .

--- Archive2025/Mar/0312_wed/src/BOJ_1914.java
+++ Archive2025/Mar/0312_wed/src/BOJ_1914.java
@@ -1,4 +1,10 @@
+import java.util.*;
 
 public class BOJ_1914 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 장대 원판 수
+		
+		sc.close();
+	}
 }
