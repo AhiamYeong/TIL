b97d8c5930bc00a742bcd140971b4adba00d83a5
AhiamYeong <toktto@gmail.com>
2025-04-02 23:53:49 +0900

    .

---
+++ Archive2025/Apr/0401_mon/Solving/src/BOJ16395.java
@@ -0,0 +1,32 @@
+/* 파스칼 삼각형: C(n-1, k-1) 출력하기
+ * 어차피 n번째는 계속 고정 
+ * dp로 재사용
+ * */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ16395 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // n번째 행
+		int K = sc.nextInt(); // k번째 수
+		
+		BigInteger[][] pascal = new BigInteger[N][N]; // N번쨰 행
+		
+		for (int i = 0; i < N; i++) {
+			// 줄별로 떼서 보기 
+			pascal[i][0] = BigInteger.ONE;
+			// 1번째행에서 어차피 1로 덮어씌워짐
+			pascal[i][i] = BigInteger.ONE;
+			
+			for (int j = 1; j < i; j++) {
+				// 왼쪽위 + 바로위 
+				pascal[i][j] = pascal[i-1][j-1].add(pascal[i-1][j]);
+			}
+		}
+		System.out.println(pascal[N-1][K-1]);
+		sc.close();
+	}
+}
--- Archive2025/Apr/0401_mon/Solving/src/BOJ2193.java
+++ Archive2025/Apr/0401_mon/Solving/src/BOJ2193.java
@@ -2,8 +2,10 @@
  * 조건1 0으로 시작하지 않아야 함
  * 조건2 1이 2번 연속으로 나타나지 않아야 함
  * 부분 문자열에 11이 없다 1
+ * 2차원배열로 끝이 0일때, 끝이 1일때 2가지 저장 
  * */
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BOJ2193 {
@@ -11,30 +13,32 @@ public class BOJ2193 {
 		// dp 테이블에 저장할 것: 경우의 수?
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] dp = new int[N+1];
-		// 상태를 어떻게 - 1 11 111
 		
-		// 반드시 가능한 경우의 수: 1자릿수에서 1이 하나
-		 
+		// 지피티 힌트 = 2차원배열로 마지막 수 0일때 + 1일때 저장 
+		BigInteger[][] dp = new BigInteger[N+1][2];
 		// 이전 자리까지 보고 저장하기? 
-		
-		// i를 자릿수로 두고 ... i자리 이친수를 구하는 함수 
-		
+
 		// 1자리에서는 1, 2자리에서는 10
-		dp[1] = 1;
-		if (N >= 2) dp[2] = 1; 
+		dp[1][0] = BigInteger.ZERO; // X
+		dp[1][1] = BigInteger.ONE; // 1
+		
+		if (N >= 2) {
+			dp[2][0] = BigInteger.ONE; // 10
+			dp[2][1] = BigInteger.ZERO; // X
+		}
 		
 		// 3번째 자리부터는 ...
-		// 근데 2번째부터 봐도 상관없지 않나? 이진수니까 들어있는건 0/1 두개 뿐
-		// 이전까지는 싹 없던거야......
-		// i-2번째 자리는 무조건 1이구
-		// i-1번째 자리가 0일 경우: 1,0 모두 가능 
-		// i-1번째가 1일 경우: 0만 가능
-		// 4번째라면 1000, 1010, 1001
-		if (N >= 3) {
-			for (int i = 3; i <= N; i++) {
-				dp[i] = dp
-			}
+		for (int i = 3; i <= N; i++) {
+			// 현재자리수 0: 이전자리 0도 되고 1도 되고 
+			dp[i][0] = dp[i-1][0].add(dp[i-1][1]);
+			
+			// 현재자리수 1: 이전자리 무조건 0
+			dp[i][1] = dp[i-1][0]; 
 		}
+
+		
+		// 결과는 0일때 경우 + 1일때 경우
+		BigInteger res = dp[N][0].add(dp[N][1]);
+		System.out.println(res);
 	}
 }
