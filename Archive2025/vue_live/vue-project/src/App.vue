4706e9dc1c032441abfcc70c63f7b3cb6e0cdf94
AhiamYeong <toktto@gmail.com>
2025-05-13 10:43:05 +0900

    0513: vue-project

---
+++ Archive2025/vue_live/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>App.vue</h2>
+    <hr />
+    <MyComponent />
+  </div>
+</template>
+
+<script setup>
+// import MyComponent from "./components/MyComponent.vue";
+import MyComponent from "@/components/MyComponent.vue";
+// @: src folder 의미 (vite.config 내에 @ 등록되어있는지 확인)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Archive2025/vue_live/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>MyComponent</h2>
+
+    <!-- v-for로 반복문 돌릴 수 있지: 해당 컴포넌트에 데이터 같이 내려줘야 함 -->
+    <MyComponentItem />
+    <my-component-item></my-component-item>
+  </div>
+</template>
+
+<script setup>
+import MyComponentItem from "./MyComponentItem.vue";
+</script>
+
+<style lang="scss" scoped></style>
