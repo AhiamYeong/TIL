faaf97eab73e597e92780d3fc19ec83596e8cd16
AhiamYeong <toktto@gmail.com>
2025-05-14 17:57:40 +0900

    0514: after class

--- Archive2025/vue_live/Vue_Day06_ComponentStateFlow/vue-project/src/components/Parent.vue
+++ Archive2025/vue_live/Vue_Day06_ComponentStateFlow/vue-project/src/components/Parent.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>부모컴포넌트</h2>
+    <!-- dynamic-props 이름으로 'name' ref 던져주기  -->
     <!-- 1. 정적바인딩 key="value": 전달할 리터럴 바로 명시 -->
     <!-- 2. 동적바인딩(v-bind로 'dynamic-props 이름으로
     'name' 만들고, ref로 바인딩 한 후, 실제 가지고있는 데이터 내려보내기 ) -->
@@ -8,7 +9,13 @@
     <input type="text" v-model="name" />
     <!-- some-event 발생 시 실행할 콜백함수 달기기 -->
     <!-- emit-args라는 이벤트 발생 시 수신해 실행할 콜백 함수가 getNumber -->
-    <parent-child my-msg="hello2" :dynamic-props="name" @some-event="someCallback" @emit-args="getNumber" @update-name="updatNameeee"></parent-child>
+    <parent-child
+      my-msg="hello2"
+      :dynamic-props="name"
+      @some-event="someCallback"
+      @emit-args="getNumber"
+      @update-name="updatNameeee"
+    ></parent-child>
   </div>
 </template>
 
