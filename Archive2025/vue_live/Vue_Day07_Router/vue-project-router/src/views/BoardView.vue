ede2176ed99ff7de9339a6d0f61f9f114bc4e13b
AhiamYeong <toktto@gmail.com>
2025-05-15 12:50:03 +0900

    0515: after lecture

--- Archive2025/vue_live/Vue_Day07_Router/vue-project-router/src/views/BoardView.vue
+++ Archive2025/vue_live/Vue_Day07_Router/vue-project-router/src/views/BoardView.vue
@@ -2,8 +2,8 @@
   <div>
     <h2>boardview</h2>
 
-    <RouterLink to="/board">게시글 목록</RouterLink>
-    <RouterLink to="/board/create">게시글 등록</RouterLink>
+    <!-- <RouterLink to="/board">게시글 목록</RouterLink>
+    <RouterLink to="/board/create">게시글 등록</RouterLink> -->
     <RouterLink :to="{ name: 'boardList' }">게시글 목록</RouterLink>
     <RouterLink :to="{ name: 'boardCreate' }">게시글 등록</RouterLink>
 
